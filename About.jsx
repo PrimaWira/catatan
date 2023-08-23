b9130b9ebda0515840850ea2b2d0ba16c44dbf0a
Pas_M.Prima <118792179+PrimaWira@users.noreply.github.com>
2023-08-23 10:02:50 +0700

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Navbar'
+// import Footer from '../Footer'
+import './About.css'
+
+const About = () => {
+  return (
+    <>
+        <Navbar/>
+
+    <div className='po'>
+        <h3>Tentang Kami</h3>
+            <p>SMK Letris indonesia 2, adalah sekolah yang telah terakreditasi A, mempunyai 7 jurusan yaitu multimedia, rekayasa perangkat lunak, teknik komputer jaringan, administrasi perkantoran, bisnis daring dan pemasaran, akuntansi, dan perbankan syariah.SMK LETRIS INDONESIA 2 akan mengadakan study tours ke Bali dengan siswa-siswi angkatan tahun 2022-2025 pada tanggal 20 Juni 2025</p>
+        </div>
+        {/* <Footer/> */}
+    </>
+  )
+}
+
+export default About
---
+++ Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar'
+// import Footer from '../Footer'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <>
+     <Navbar />
+        <div className='op'>
+            <h1>Contact</h1>
+        </div>
+     {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Contact
